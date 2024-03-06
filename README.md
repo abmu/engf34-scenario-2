f5c1044f167733aa5cfa0f0483b4d2c0c7a47123
Abdul Muhaymin Abdul Hafiz <mr-abdul@live.co.uk>
2024-03-06 19:28:45 +0000

    Get API response

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # engf34-scenario-2
+
+## Setup
+
+Install the requirements
+
+```
+pip install -r requirements.txt
+```
+
+Create a `.env` file, and then add the following line
+
+```
+OPENAI_KEY="{add key here}"
+```
--- main.py
+++ main.py
@@ -1,12 +1,33 @@
-import openai
-import os
-from dotenv import load_dotenv
+from openai_api import OpenAIAPI
 
-class OpenAIAPI:
-    def __init__(self, api_key: str=None) -> None:
-        if api_key is None:
-            load_dotenv()
-            api_key = os.getenv('OPENAI_KEY')
-        openai.api_key = api_key
-        self._custom_instruction: str = None
+def exit_program(gpt_api: OpenAIAPI) -> None:
+    exit()
 
+def set_custom(gpt_api: OpenAIAPI) -> None:
+    custom_instruction = input('Enter custom instruction > ')
+    gpt_api.set_custom_instruction(custom_instruction)
+
+COMMANDS = {
+    'exit': exit_program,
+    'setcustom': set_custom
+}
+
+def main():
+    gpt_api = OpenAIAPI()
+    while True:
+        user_input = input('> ')
+
+        # handle commands
+        if user_input[:1] == '/':
+            command = user_input[1:]
+            if command not in COMMANDS:
+                print('Invalid command!')
+            else:
+                COMMANDS[command](gpt_api)
+            continue
+
+        response = gpt_api.get_response(user_input)
+        print(f'gpt-3.5-turbo: {response}')
+
+if __name__ == '__main__':
+    main()
